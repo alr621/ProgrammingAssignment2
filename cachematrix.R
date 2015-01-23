c5ecff8021ae706ad68ac847a40a70d4d93aa359
Amanda Richards <amanda.richards@datamyx.com>
2015-01-23 11:21:00 -0500

    cache matrix

--- cachematrix.R
+++ cachematrix.R
@@ -5,11 +5,32 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  a<-NULL
+  set<-function(b) {
+    x<<-b
+    a<<-NULL
+  }
+  get<-function() x
+  setinv<-function(solve) a <<- solve
+  getinv<-function() a
+  list(set=set,get=get,setinv=setinv,getinv=getinv)
+
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  a<-x$getinv()
+  if(!is.null(a)) {
+    message("Found cached data.")
+    return(a)
+  }
+  else {
+    message("No cached data found. Calculating inverse matrix.")
+    data<-x$get()
+    a<-solve(data)
+    x$setinv(a)
+    return(a)
+  }
 }
